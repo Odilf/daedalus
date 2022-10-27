adeecee06784eab36ca4968fd60ec66a47d28ca1
Odilf <odysseas.maheras@gmail.com>
2022-10-27 21:14:43 +0200

    YEESSSSS

--- src/render/cache_rays.s
+++ src/render/cache_rays.s
@@ -15,28 +15,23 @@ angle_delta: .double 0.01 # in radians
 cache_column_lengths:
 	push %r15
 
-	movsd angle, %xmm8
+	movsd angle, %xmm12
 	mov $0, %r15
 
 cache_column_loop:
-	addsd angle_delta, %xmm8 # Increment angle
+	addsd angle_delta, %xmm12 # Increment angle
 
 	# Do the raycasting calculation with the angle
+	movsd %xmm12, %xmm8 # Input
 	call raycast # Output is stored on %rax
 
-	# mov $401, %rax
-
-	# mov $0, %rsi
-	# Convert raycast length to column size
+	# Convert from raycast length to column size
 	cvtsi2sd %rax, %xmm9
 	movsd one, %xmm10
 	divsd %xmm9, %xmm10
 	mulsd column_height_scalar, %xmm10
-	# maxss max_column_height, %xmm10		# Clamp to 32
-
-	# movsd idk, %xmm10
+	maxss max_column_height, %xmm10		# Clamp to 32
 	cvttsd2si %xmm10, %rsi
-
 
 	# Store the resulting column size in column_length_cache
 	movb %sil, column_length_cache(%r15)
